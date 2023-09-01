f77d682201a4f61292564f3319189c2d2ee3b371
chochey <30965960+chochey@users.noreply.github.com>
2023-09-01 16:44:15 -0400

    s

--- dashboard_script.js
+++ dashboard_script.js
@@ -121,16 +121,17 @@ document.addEventListener("DOMContentLoaded", async () => {
       },
     });
 
-    // ----------------------------------------------------------------------------------
-    // Render bar Chart for Location of Death
-    const ctx3 = document.getElementById("locationChart").getContext("2d");
-    new Chart(ctx3, {
+    // Render bar Chart for TEAM KILLS BY KILLERr
+    const ctx5 = document
+      .getElementById("teamKillsByKillerChart")
+      .getContext("2d");
+    new Chart(ctx5, {
       type: "bar",
       data: {
-        labels: Object.keys(locationData),
+        labels: Object.keys(teamKillsByKillerData),
         datasets: [
           {
-            data: Object.values(locationData),
+            data: Object.values(teamKillsByKillerData),
             backgroundColor: [
               "rgba(255, 99, 132, 0.2)",
               "rgba(54, 162, 235, 0.2)",
@@ -148,77 +149,16 @@ document.addEventListener("DOMContentLoaded", async () => {
         ],
       },
     });
-  } catch (error) {
-    console.error("Error fetching and processing data:", error);
-  }
-});
-
-// Render bar Chart for TEAM KILLS BY PLAYER
-const ctx4 = document.getElementById("teamKillsByPlayerChart").getContext("2d");
-new Chart(ctx4, {
-  type: "bar",
-  data: {
-    labels: Object.keys(teamKillsByPlayerData),
-    datasets: [
-      {
-        data: Object.values(teamKillsByPlayerData),
-        backgroundColor: "rgba(255, 99, 132, 0.2)",
-        borderColor: "rgba(255, 99, 132, 1)",
-        borderWidth: 1,
-      },
-    ],
-  },
-});
-
-// Render bar Chart for TEAM KILLS BY KILLER
-const ctx5 = document.getElementById("teamKillsByKillerChart").getContext("2d");
-new Chart(ctx5, {
-  type: "bar",
-  data: {
-    labels: Object.keys(teamKillsByKillerData),
-    datasets: [
-      {
-        data: Object.values(teamKillsByKillerData),
-        backgroundColor: "rgba(54, 162, 235, 0.2)",
-        borderColor: "rgba(54, 162, 235, 1)",
-        borderWidth: 1,
-      },
-    ],
-  },
-});
-
-document.addEventListener("DOMContentLoaded", async () => {
-  try {
-    const response = await fetch("/api/get_teamkills");
-    const teamKillsData = await response.json();
-
-    // ... [Your existing data processing code for other charts here]
-
-    // Process data for TEAM KILLS BY KILLER
-    const teamKillsByKillerData = {};
-    teamKillsData.forEach((kill) => {
-      const killer = kill[1]; // Assuming killer name is in Column B
-      teamKillsByKillerData[killer] = (teamKillsByKillerData[killer] || 0) + 1;
-    });
-
-    // Process data for TEAM KILLS BY PLAYER
-    const teamKillsByPlayerData = {};
-    teamKillsData.forEach((kill) => {
-      const player = kill[0]; // Assuming player name is in Column A
-      teamKillsByPlayerData[player] = (teamKillsByPlayerData[player] || 0) + 1;
-    });
-
-    // Render bar Chart for TEAM KILLS BY KILLERr
-    const ctx5 = document
-      .getElementById("teamKillsByKillerChart")
-      .getContext("2d");
-    new Chart(ctx5, {
+    // ----------------------------------------------------------------------------------
+    // Render bar Chart for Location of Death
+    const ctx3 = document.getElementById("locationChart").getContext("2d");
+    new Chart(ctx3, {
       type: "bar",
       data: {
-        labels: Object.keys(teamKillsByKillerData),
+        labels: Object.keys(locationData),
         datasets: [
           {
-            data: Object.values(teamKillsByKillerData),
+            data: Object.values(locationData),
             backgroundColor: [
               "rgba(255, 99, 132, 0.2)",
               "rgba(54, 162, 235, 0.2)",
