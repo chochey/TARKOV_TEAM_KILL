505f2d34c5bb01e5eca0a73eae615001379d460b
chochey <30965960+chochey@users.noreply.github.com>
2023-09-01 16:14:16 -0400

    s

--- dashboard_script.js
+++ dashboard_script.js
@@ -108,3 +108,51 @@ document.addEventListener("DOMContentLoaded", async () => {
     console.error("Error fetching and processing data:", error);
   }
 });
+
+// Process data for TEAM KILLS BY PLAYER
+const teamKillsByPlayerData = {};
+teamKillsData.forEach((kill) => {
+  const player = kill[0]; // Assuming player name is in Column A
+  teamKillsByPlayerData[player] = (teamKillsByPlayerData[player] || 0) + 1;
+});
+
+// Render bar Chart for TEAM KILLS BY PLAYER
+const ctx4 = document.getElementById("teamKillsByPlayerChart").getContext("2d");
+new Chart(ctx4, {
+  type: "bar",
+  data: {
+    labels: Object.keys(teamKillsByPlayerData),
+    datasets: [
+      {
+        data: Object.values(teamKillsByPlayerData),
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 1,
+      },
+    ],
+  },
+});
+
+// Process data for TEAM KILLS BY KILLER
+const teamKillsByKillerData = {};
+teamKillsData.forEach((kill) => {
+  const killer = kill[1]; // Assuming killer name is in Column B
+  teamKillsByKillerData[killer] = (teamKillsByKillerData[killer] || 0) + 1;
+});
+
+// Render bar Chart for TEAM KILLS BY KILLER
+const ctx5 = document.getElementById("teamKillsByKillerChart").getContext("2d");
+new Chart(ctx5, {
+  type: "bar",
+  data: {
+    labels: Object.keys(teamKillsByKillerData),
+    datasets: [
+      {
+        data: Object.values(teamKillsByKillerData),
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+        borderColor: "rgba(54, 162, 235, 1)",
+        borderWidth: 1,
+      },
+    ],
+  },
+});
