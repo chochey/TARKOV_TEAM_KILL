6e8b1a13b9135cfaba9a540a76612cddfdd74a08
chochey <30965960+chochey@users.noreply.github.com>
2023-09-01 15:49:41 -0400

    h

--- dashboard_script.js
+++ dashboard_script.js
@@ -1,77 +1,110 @@
-
 document.addEventListener("DOMContentLoaded", async () => {
-    try {
-        const response = await fetch("/api/get_teamkills");
-        const teamKillsData = await response.json();
+  try {
+    const response = await fetch("/api/get_teamkills");
+    const teamKillsData = await response.json();
 
-        // Process data for team kills over time
-        const teamKillsOverTime = {};
-        teamKillsData.forEach(kill => {
-            const date = kill[5]; // Assuming 6th column is the date
-            teamKillsOverTime[date] = (teamKillsOverTime[date] || 0) + 1;
-        });
+    // Process data for What killed you
+    const causeOfDeathData = {};
+    teamKillsData.forEach((kill) => {
+      const cause = kill[2]; // Column C
+      causeOfDeathData[cause] = (causeOfDeathData[cause] || 0) + 1;
+    });
 
-        // Process data for team kills by player
-        const teamKillsByPlayer = {};
-        teamKillsData.forEach(kill => {
-            const player = kill[0]; // Assuming 1st column is the player
-            teamKillsByPlayer[player] = (teamKillsByPlayer[player] || 0) + 1;
-        });
+    // Process data for Map
+    const mapData = {};
+    teamKillsData.forEach((kill) => {
+      const map = kill[3]; // Column D
+      mapData[map] = (mapData[map] || 0) + 1;
+    });
 
-        // Process data for team kills by killer
-        const teamKillsByKiller = {};
-        teamKillsData.forEach(kill => {
-            const killer = kill[1]; // Assuming 2nd column is the killer
-            teamKillsByKiller[killer] = (teamKillsByKiller[killer] || 0) + 1;
-        });
+    // Process data for Location of Death
+    const locationData = {};
+    teamKillsData.forEach((kill) => {
+      const location = kill[4]; // Column E
+      locationData[location] = (locationData[location] || 0) + 1;
+    });
 
-        // Render Bar Chart for Team Kills Over Time
-        const ctx1 = document.getElementById("teamKillsOverTimeChart").getContext("2d");
-        new Chart(ctx1, {
-            type: 'bar',
-            data: {
-                labels: Object.keys(teamKillsOverTime),
-                datasets: [{
-                    label: 'Team Kills',
-                    data: Object.values(teamKillsOverTime),
-                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                    borderColor: 'rgba(255, 99, 132, 1)',
-                    borderWidth: 1
-                }]
-            }
-        });
+    // Render Pie Chart for What Killed You
+    const ctx1 = document.getElementById("causeOfDeathChart").getContext("2d");
+    new Chart(ctx1, {
+      type: "pie",
+      data: {
+        labels: Object.keys(causeOfDeathData),
+        datasets: [
+          {
+            data: Object.values(causeOfDeathData),
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+    });
 
-        // Render Pie Chart for Team Kills by Player
-        const ctx2 = document.getElementById("teamKillsByPlayerChart").getContext("2d");
-        new Chart(ctx2, {
-            type: 'pie',
-            data: {
-                labels: Object.keys(teamKillsByPlayer),
-                datasets: [{
-                    data: Object.values(teamKillsByPlayer),
-                    backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)'],
-                    borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)'],
-                    borderWidth: 1
-                }]
-            }
-        });
+    // Render Pie Chart for Map
+    const ctx2 = document.getElementById("mapChart").getContext("2d");
+    new Chart(ctx2, {
+      type: "pie",
+      data: {
+        labels: Object.keys(mapData),
+        datasets: [
+          {
+            data: Object.values(mapData),
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+    });
 
-        // Render Pie Chart for Team Kills by Killer
-        const ctx3 = document.getElementById("teamKillsByKillerChart").getContext("2d");
-        new Chart(ctx3, {
-            type: 'pie',
-            data: {
-                labels: Object.keys(teamKillsByKiller),
-                datasets: [{
-                    data: Object.values(teamKillsByKiller),
-                    backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)'],
-                    borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)'],
-                    borderWidth: 1
-                }]
-            }
-        });
-
-    } catch (error) {
-        console.error("Error fetching and processing data:", error);
-    }
+    // Render Pie Chart for Location of Death
+    const ctx3 = document.getElementById("locationChart").getContext("2d");
+    new Chart(ctx3, {
+      type: "pie",
+      data: {
+        labels: Object.keys(locationData),
+        datasets: [
+          {
+            data: Object.values(locationData),
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching and processing data:", error);
+  }
 });
