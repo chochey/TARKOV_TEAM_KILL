b5a7e47adee184b9ded0941baf5a58ea908e7027
chochey <30965960+chochey@users.noreply.github.com>
2023-09-18 23:28:37 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -92,6 +92,12 @@ function groupByPlayer(data) {
   }, {});
 }
 
+for (let entry of groupedByPlayer) {
+  if (entry.datetime) {
+    entry.date = entry.datetime.split(" ")[0];
+  }
+}
+
 function displayData(groupedData) {
   const container = document.getElementById("dataContainer");
   container.innerHTML = "";
